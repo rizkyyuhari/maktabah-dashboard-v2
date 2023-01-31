913c08a6f9c05a8c6e0a884d1876ada3596f8159
Mrizkyy <79984517+rizkyyuhari@users.noreply.github.com>
2023-01-31 17:11:09 +0700

    Update apiClient.js

--- src/network/apiClient.js
+++ src/network/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `https://54c2-2407-0-3002-146-6dab-a18a-a4e8-e67a.ap.ngrok.io`,
+  baseURL: `https://a1f0-2407-0-3002-146-6dab-a18a-a4e8-e67a.ap.ngrok.io`,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
